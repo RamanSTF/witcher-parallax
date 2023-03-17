7863fa489d3d111df1594ac9a4a0799b6eb33456
RamanSTF <plchsnik@gmail.com>
2023-03-17 10:49:29 +0100

    project ready

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+	// direction: 'vertical'
+	freeMode: true,
+	speed: 2400,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.0
+	},
+	spaceBetween: 18,
+	parallax: true,
+})
